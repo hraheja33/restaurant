406e80eedbbb8ac59da510e73daabf7ad1030e69
hraheja33 <89431410+hraheja33@users.noreply.github.com>
2021-09-16 00:28:23 -0400

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+// document.querySelector(".navbar-nav").children[0].firstElementChild.href = "home.html";
+// document.querySelector(".navbar-nav").children[2].firstElementChild.href = "menu.html";
+document.querySelector(".navbar-nav").children[2].firstElementChild.href = "galary.html";
+// document.querySelector(".navbar").children[0].href = "home.html";
